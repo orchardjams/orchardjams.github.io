465cba248c07f6a1a8664e93886d5c863e02d581
Dominic Bradbury <88463327+orchardjams@users.noreply.github.com>
2021-08-06 00:10:42 +0000

    Text colour

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.use(express.static("."));
 app.listen(process.env.PORT || "8080", () => {
-    console.log("asdf");
+    console.log("Done");
 });
-console.log("h");
+console.log("Starting Server...");
